899393132a98375b51c92601a9cece7913ef96ab
Greg Sithole <gregsithole@hotmail.co.za>
2024-05-21 23:30:26 +0200

    Added external Data Provider

---
+++ src/providers/data/index.tsx
@@ -0,0 +1,18 @@
+import { GraphQLClient } from "@refinedev/nestjs-query";
+import { fetchWrapper } from "./fetch-wrapper";
+
+export const API_URL = "http://api.crm.refine.dev";
+
+/**
+ * The GraphQL client used for making API requests.
+ * @type {GraphQLClient}
+ */
+export const client = new GraphQLClient(API_URL, {
+	fetch: (url: string, options: RequestInit) => {
+		try {
+			return fetchWrapper(url, options);
+		} catch (error) {
+			return Promise.reject(error as Error);
+		}
+	}
+});
