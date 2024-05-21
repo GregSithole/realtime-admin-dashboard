bf01f3ddef10b5c9716ecf7eb3b015e739fe182e
Greg Sithole <gregsithole@hotmail.co.za>
2024-05-21 23:38:34 +0200

    Added Web Socket Client to DataProvider

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,7 +1,10 @@
-import { GraphQLClient } from "@refinedev/nestjs-query";
+import graphqlDataProvider, { GraphQLClient, liveProvider as graphqlLiveProvider } from "@refinedev/nestjs-query";
 import { fetchWrapper } from "./fetch-wrapper";
+import { createClient } from "graphql-ws";
 
-export const API_URL = "http://api.crm.refine.dev";
+export const API_BASE_URL = "https://api.crm.refine.dev";
+export const API_URL = "https://api.crm.refine.dev";
+export const WS_URL = "wss://api.crm.refine.dev/graphql";
 
 /**
  * The GraphQL client used for making API requests.
@@ -16,3 +19,28 @@ export const client = new GraphQLClient(API_URL, {
 		}
 	}
 });
+
+/**
+ * WebSocket client for real-time data communication.
+ * 
+ * @remarks
+ * This client is created using the `createClient` function from the `graphql-ws` library.
+ * It establishes a WebSocket connection to the specified URL and includes the access token
+ * in the connection parameters for authentication.
+ * 
+ * @public
+ */
+export const wsClient = typeof window !== "undefined" ? createClient({
+	url: WS_URL,
+	connectionParams: () => {
+		const accessToken = localStorage.getItem("accessToken");
+		return {
+			headers: {
+				Authorization: `Bearer ${accessToken}`,
+			}
+		}
+	}
+}) : undefined;
+
+export const dataProvider = graphqlDataProvider(client);
+export const liveProvider = wsClient ? graphqlLiveProvider(wsClient) : undefined;
