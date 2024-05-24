33bad7ba24571f3af5c63c4dd088704c283c1842
Greg Sithole <gregsithole@hotmail.co.za>
2024-05-24 20:36:07 +0200

    Updated Login Page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,13 @@
 import { AuthPage } from "@refinedev/antd";
+import { authCredentials } from "../../providers";
 
 export const Login = () => {
-  return (
-    <AuthPage
-      type="login"
-      formProps={{
-        initialValues: { email: "demo@refine.dev", password: "demodemo" },
-      }}
-    />
-  );
+	return (
+		<AuthPage
+			type="login"
+			formProps={{
+				initialValues: authCredentials,
+			}}
+		/>
+	);
 };
