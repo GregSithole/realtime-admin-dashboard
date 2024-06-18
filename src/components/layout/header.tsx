59a80e71b6caf5b96aca5d2050521a5be8ff3f2c
Greg Sithole <gregsithole@hotmail.co.za>
2024-06-18 21:56:31 +0200

    Updated Header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,25 @@
+import { Layout, Space } from "antd"
 import CurrentUser from "./currentUser"
 
 const Header = () => {
+
+	const headerStyles: React.CSSProperties = {
+		background: '#fff',
+		display: 'flex',
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+		padding: '0 24px',
+		position: 'sticky',
+		top: 0,
+		zIndex: 999,
+	};
+
 	return (
-		<div>
-			<CurrentUser />
-		</div>
+		<Layout.Header style={headerStyles}>
+			<Space align="center" size="middle">
+				<CurrentUser />
+			</Space>
+		</Layout.Header>
 	)
 }
 
