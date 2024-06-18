1ddf7df5304a362a61178b98bf82ca7f73a82cce
Greg Sithole <gregsithole@hotmail.co.za>
2024-06-18 21:44:28 +0200

    Added Utils functions

---
+++ src/utilities/index.ts
@@ -0,0 +1,6 @@
+// @index('./**/*.ts', f => `export * from '${f.path}'`)
+export * from './currencyNumber'
+export * from './dates'
+export * from './getNameInitials'
+export * from './getRandomColorFromString'
+// @endindex
