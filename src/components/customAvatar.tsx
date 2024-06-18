f37d66ec1bb14ab676e9a257fd2cff99b5ada917
Greg Sithole <gregsithole@hotmail.co.za>
2024-06-18 21:44:45 +0200

    Updated Avatar Component

--- src/components/customAvatar.tsx
+++ src/components/customAvatar.tsx
@@ -1,13 +1,15 @@
+
+import { getNameInitials } from "@/utilities"
 import { Avatar as AntdAvatar, AvatarProps } from "antd"
 
 type Props = AvatarProps & {
-	name: string,
+	name?: string,
 }
 
 const CustomAvatar = ({ name, style, ...rest }: Props) => {
 	return (
-		<AntdAvatar alt={'Greg Sithole'} size="small" style={{ backgroundColor: '#87d068', display: 'flex', alignItems: 'center', border: 'none' }}>
-			GS
+		<AntdAvatar alt={name} size="small" style={{ backgroundColor: '#87d068', display: 'flex', alignItems: 'center', border: 'none', ...style }} {...rest}>
+			{getNameInitials(name || '')}
 		</AntdAvatar>
 	)
 }
