a82cd84b7980c29b96b993e24dbf0ff4380638f6
Greg Sithole <gregsithole@hotmail.co.za>
2024-07-31 22:15:44 +0200

    Fixed Route Replacement

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -9,11 +9,14 @@ import { TASK_STAGES_QUERY, TASKS_QUERY } from "@/graphql/queries"
 import { TaskStage } from "@/graphql/schema.types"
 import { TasksQuery } from "@/graphql/types"
 import { DragEndEvent } from "@dnd-kit/core"
-import { useList, useUpdate } from "@refinedev/core"
+import { useList, useNavigation, useUpdate } from "@refinedev/core"
 import { GetFieldsFromList } from "@refinedev/nestjs-query"
 import React from "react"
 
 const TaskList = ({ children }: React.PropsWithChildren) => {
+
+	const { replace } = useNavigation();
+
 	const { data: stages, isLoading: isLoadingStages } = useList<TaskStage>({
 		resource: 'taskStages',
 		filters: [
@@ -76,7 +79,8 @@ const TaskList = ({ children }: React.PropsWithChildren) => {
 	}, [stages, tasks]);
 
 	const handleAddCard = (args: { stageId: string }) => {
-
+		const path = args.stageId === 'unassigned' ? '/tasks/new' : `/tasks/new?stageId=${args.stageId}`;
+		replace(path);
 	}
 
 	const handleOnDragEnd = (event: DragEndEvent) => {
